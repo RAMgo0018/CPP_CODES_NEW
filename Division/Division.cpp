a1c5b2d9d972c5ebbb395668f70957dbaeb11016
Ram Goyal <ramgoyal2005@gmail.com>
2024-04-19 19:50:54 +0530

    Division Code Added

---
+++ Division/Division.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x,y;
+    cout<< " enter x"<< endl;
+    cin>> x;
+    cout<<" enter y"<< endl;
+    cin>>y;
+    cout<< "multiplication is "<<(x/y);
+    return 0;
+}
